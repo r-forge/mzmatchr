f11264e07358d9ef7aad2407428bd61ad73d16ae
ajankevics <ajankevics@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-10-22 18:09:26 +0000

    Updated data files.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mzmatchr@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mzmatch.Data/R/zzz.R
+++
@@ -1,6 +0,0 @@
-##
-## Repair path on other platforms
-##
-attr(xs, "filepaths") <- file.path(.find.package("mzmatch.Data"),
-                                 gsub("/", .Platform$file.sep,
-                                      attr(xs, "filepaths")))
