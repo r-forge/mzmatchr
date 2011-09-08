1138c7da8819d48a952174f147b08120397c506b
cbaunni (Dr ) <unni.chokkathukalam@glasgow.ac.uk>
2011-09-08 12:16:15 +0000

    PeakML.Methods getProtonMass and getProtonCoeff Added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mzmatchr@36 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/mzmatch.R/R/PeakML.Methods.getProtonCoef.R
@@ -0,0 +1,19 @@
+PeakML.Methods.getProtonCoef <- function(javaProject, ionisation="detect"){
+	# PRE:
+	# 	Java project, inonisation (NULL, detect, negative, positive)
+	# POST:
+	#	protonCoef, mass correction factor
+	
+	#Detect ionisation mode, only first scan from first sample is used, no ionisation switching is supported yet
+	if (ionisation=="detect") {
+		ionisation<- .jcall (javaProject,return="S",method="getIonisation",as.integer(0),as.integer(0))
+	}
+
+	if (ionisation=="positive") {
+		protonCoef <- 1
+	} else if (ionisation=="negative") {
+		protonCoef <- -1
+	} else	protonCoef <- 0
+	
+	protonCoef
+}
