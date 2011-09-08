0309e6e94d9900aec547babdd4a6cb2e8e042e16
cbaunni <cbaunni@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-08 15:13:01 +0000

    PeakML.Methonds getPeakData and getMassCorrection added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mzmatchr@39 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/mzmatch.R/R/PeakML.Methods.getMassCorrection.R
@@ -0,0 +1,11 @@
+PeakML.Methods.getMassCorrection <- function(javaProject, ionisation="detect"){
+	# PRE:
+	# 	the jave project, ionisation mode (see PeakML.Methods.getProtonCoef)
+	# POST:
+	#	returns mass correction as numeric
+	
+	protonMass <- PeakML.Methods.getProtonMass()
+	protonCoef <- PeakML.Methods.getProtonCoef(javaProject, ionisation)
+	rv <- protonMass * protonCoef
+	rv
+}
