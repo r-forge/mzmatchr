53c32428986421f212e399023c55853036fcf067
cbaunni (Dr ) <unni.chokkathukalam@glasgow.ac.uk>
2011-11-12 17:31:59 +0000

    all methods of the targetted isotope picking are now online
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mzmatchr@56 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/mzmatch.R/R/PeakML.Isotope.TargettedIsotopes.R
@@ -0,0 +1,57 @@
+PeakML.Isotope.TargettedIsotopes <- function(baseDir, sampleType = "NEG", molFormulaFile, outDirectory = "targettedIsotops", outFileName, mzXMLSrc=NULL, peakMLFile, sampleGroups = NULL, layoutMtx = NULL, 
+		massCorrection, ppm = 2, trendPlots = NULL, loadSavedData = TRUE, fillGaps, useArea=FALSE, stdRTWindow=NULL){
+	# PRE:
+	#	peakMLFiles: the complete peakml dataset
+	#	molFormulaFile: file containing the list of molecules whoes isotops has to be found out
+	#	outDirectory: is the outDirectory where the output has to be saved
+	#	mzXMLSrc: is the source of the mzXML file is not in the current working outDirectory
+	#	include_ionisation: not necessary in this cases
+	#	ionisation: set this if include_ionisation=TRUE
+	#	loadSavedData: load from the saved peakml file
+	# 	sampleType: the sample type eg. NEG, POS etc
+	# POST:
+	#	vector containing the list of isotops
+	
+	## Reads the peakml file & prepare the parameters to scan for isotops
+	## --------------------------------------------------------------------
+	cat("Indentifying isotops in ", sampleType, " sample\n")
+	setwd (paste(baseDir, sampleType, sep="/"))
+	
+	if (!is.null(mzXMLSrc)) mzXMLSrc <- paste(mzXMLSrc, sampleType, sep="/")
+	
+	if (loadSavedData == TRUE){
+		load("cpData.Rdata")
+	} else{
+		chromPeakData <- PeakML.Read(peakMLFile, ionisation = "neutral")
+		save("chromPeakData", file="cpData.Rdata")
+	}
+	
+	peakDataMtx <- chromPeakData[[1]]
+	chromDataList <- chromPeakData[[2]]
+	sampleClasses <- chromPeakData[[3]]
+	sampleNames <- chromPeakData[[4]]
+	phenoData <- PeakML.Methods.getPhenoData(sampleClasses, sampleNames, peakDataMtx)
+
+	if (sampleType == "NEG"){
+		massCorrection <- -PeakML.Methods.getProtonMass()
+	} else if (sampleType == "POS"){
+		massCorrection <- +PeakML.Methods.getProtonMass()
+	}
+
+	if (is.null(sampleGroups)) sampleGroups <- unique(phenoData)		# To enable the user to change the order of the samples
+	if (is.null(trendPlots)) trendPlots <- c("RATIO","TREND", "LABELLED")
+	
+	if (length(sampleGroups)>10) {
+		if (is.null(layoutMtx)) stop("You have more than 10 samples to plot. Please specify an appropriate layout matrix.\n") else plotOrder <- c(sampleGroups, trendPlots)
+	} else {
+		if (is.null(layoutMtx)) layoutMtx <- matrix(c(1,1,1,1,2, 3,4,5,6,7, 8,9,10,11,12, 13 ,14,14, 15,15),4,5, byrow=TRUE)
+		if (length(sampleGroups)<10) plotOrder <- c(sampleGroups, rep("EMPTY", 10-length(sampleGroups)), trendPlots)
+		if (length(sampleGroups)==10) plotOrder <- c(sampleGroups, trendPlots)
+	}
+	
+	PeakML.Isotope.processTargettedIsotopes(molFormulaFile, outDirectory, outFileName, layoutMtx, ppm, stdRTWindow, sampleType,
+	sampleNames, peakDataMtx, chromDataList, phenoData, sampleGroups, plotOrder, mzXMLSrc, 
+	fillGaps, massCorrection, useArea)
+
+}
+
---
+++ pkg/mzmatch.R/R/PeakML.Isotope.plotSamples.R
@@ -0,0 +1,137 @@
+
+PeakML.Isotope.plotSamples <- function(isotopeChroms, metName, metFormula, metMass, stdRT, sampleType, sampleGroups, plotOrder, useArea, followCarbon){
+#plotSamples <- function(isotopeChroms, plotOrder, sampleGroups, plotText, useArea, followCarbon){
+
+	mzList <- isotopeChroms[[1]]
+	intList <- isotopeChroms[[2]]
+	rtList <- isotopeChroms[[3]]
+	
+	numPeakGroups <- length(mzList)
+	numSampleGroups <- length(mzList[[1]])
+	numCarbons <- length(mzList[[1]][[1]])
+	numReplicates <- length(mzList[[1]][[1]][[1]])
+	
+#	metName <- plotText[1]
+	metName <- unlist(strsplit(as.character(metName), ", "))
+#	metFormula <- plotText[2]
+#	metMass <- as.numeric(plotText[4])
+#	sampleType <-  plotText[9]
+#	stdRT <- plotText[7]
+	if(stdRT==0) stdRT<-NA
+	
+	fillLabels <- c("UL" , paste(as.character("+"),c(1:numCarbons),sep=""))# create a list of isotop names
+	fillColor <- c(1:numCarbons)
+	if (numCarbons > 7) fillColor <- c("black", rainbow(numCarbons, alpha=0.5))
+	
+	for (peakGroup in 1:numPeakGroups){
+		trendList <- PeakML.Isotope.getTrendList (intList, sampleGroups, useArea) [[peakGroup]]
+		ratioMtx <- PeakML.Isotope.getRatioMtxList (intList, sampleGroups, useArea, metName) [[peakGroup]]
+		
+		par (mar=c(0,0,0,0))
+		plot (c(1:10), c(1:10), xlab="", ylab="", pch="", axes=F) 
+		
+		if(length(metName)==1){
+			text(1.5, 6, metName[1], cex = 2, pos=4)
+			text(1.5, 4, paste("Formula:", metFormula, "Mass:", round(metMass,3), "Std.RT:", stdRT, "Ion:", sampleType, sep = "  "), pos=4, cex = 1.5)
+		} else {
+			text(1.5, 7, metName[1], cex = 2, pos=4)
+			text(1.5, 5, paste(metName[2:length(metName)], collapse=","), cex = .5, pos=4)
+			text(1.5, 4, paste("Formula:", metFormula, "Mass:", round(metMass,3), "Std.RT:", stdRT, "Ion:", sampleType, sep = "  "), pos=4, cex = 1)
+		}
+		legend(1.5, 3,fill=fillColor, fillLabels[1:numCarbons], bty="n", horiz=TRUE)
+		
+		plot (1, 1, xlab="", ylab="", pch="", axes=F)
+		text(1, 1, paste("G", as.character(peakGroup), sep=""), cex = 3)
+	
+		for (item in 1:length(plotOrder)){
+			if(plotOrder[item] == "LEGEND"){
+
+				par(mar=c(0.5,0.5,0.5,0.5))
+				plot (1, 1, xlab="", ylab="", pch="", axes=F)
+				legend("topleft",fill=fillColor, fillLabels[1:numCarbons], bty="n")
+
+			} else if (plotOrder[item] == "TREND"){
+			
+				errBar <- function(x, y, upper, lower=upper, length=0.1,...){
+					if(length(x) != length(y) | length(y) !=length(lower) | length(lower) != length(upper))
+					stop("vectors must be same length")
+					arrows(x, y+upper, x, y-lower, angle=90, code=3, length=length, ...)
+				}
+				
+				trendMtx <- PeakML.Isotope.getTrendMtx(trendList, sampleGroups)
+				trendMtx.sum <- apply(trendMtx, 2, sum)
+				trendMtx.sd <- apply(trendMtx, 2, sd)
+				ylimit <- sum(max(trendMtx.sum), max(trendMtx.sd))
+				# calculate std err
+				errMtx <- replace(trendMtx, trendMtx==0, NA)
+				stdErr <- apply(errMtx, 2, PeakML.Methods.getStdErr)
+
+				par(mar=c(4,2.5,0.5,3))
+				if (useArea == FALSE){
+					ylabel <- paste(metName[1], "(mean peak height)", sep= " ")
+				} else {
+					ylabel <- paste(metName[1], "(mean peak area)", sep= " ")
+				}
+
+				barx <- barplot(trendMtx, beside=FALSE, col=fillColor, ylab=ylabel,ylim=c(0,ylimit), border=NA, axisnames=FALSE)
+				axis(1, las=2, at=c(1:length(sampleGroups)), lab=sampleGroups, lwd=0, cex.axis=.8)
+				errBar(barx, trendMtx.sum, trendMtx.sd/numCarbons, lwd=.3)
+
+			} else if (plotOrder[item] == "LABELLED"){
+			
+				if(is.na(followCarbon)) followCarbon <- numCarbons
+
+				fcMtx <- PeakML.Isotope.getFCMtx (trendList, sampleGroups, followCarbon)
+
+				if (useArea == FALSE){
+					ylabel <- paste(metName[1], "(mean peak height)", sep= " ")
+				} else {
+					ylabel <- paste(metName[1], "(mean peak area)", sep= " ")
+				}
+
+				ylimit <- max(fcMtx)
+				if (!ylimit==0){
+					par(mar=c(4,2.5,0.5,3))
+					barplot(fcMtx, beside=FALSE, col=fillColor[followCarbon], axisnames=FALSE, ylab=ylabel, ylim=c(0,ylimit), border=NA)
+					axis(1, las=3, at=c(1:length(sampleGroups)), lab=sampleGroups, lwd=0, cex.axis=.8)
+				} else {
+					par(mar=c(0.5,0.5,0.5,0.5))
+					plot (1, 1, xlab="", ylab="", pch="", axes=F)
+				}
+
+			} else if (plotOrder[item] == "RATIO") {
+
+				par(mar=c(4,4,3,2))
+				barplot(t(ratioMtx), beside=FALSE, col=fillColor, ylab="% area under peak", ylim = c(0,1), border=NA, axisnames=FALSE)
+				axis(1, las=3, at=c(1:length(row.names(ratioMtx))), lab=row.names(ratioMtx), lwd=0, cex.axis=.5)
+				
+			} else if (plotOrder[item] == "EMPTY"){
+
+				# This is a an empty plot for flexibility e.g. define empty if a plot is not needed
+				plot (1, 1, xlab="", ylab="", pch="", axes=F)
+			} else{
+				if (! length(unlist(rtList[[peakGroup]][[item]])) == 0){
+					maxRT <- max(unlist(rtList[[peakGroup]][[item]]),na.rm=TRUE)
+					minRT <- min(unlist(rtList[[peakGroup]][[item]]),na.rm=TRUE)
+					maxIN <- max(unlist(intList[[peakGroup]][[item]]),na.rm=TRUE)
+					minIN <- 0
+				}else{
+					maxRT <- 0
+					minRT <- 0
+					maxIN <- 0
+					minIN <- 0
+				}
+
+				par(mar=c(4,4,0.5,0.5), mgp=c(1.5,0.5,0))
+				plot (1, 1, pch="", xlab="", ylab="", xlim=c(minRT,maxRT), ylim=c(minIN,maxIN), cex.axis=0.75)
+				for (isotop in 1:numCarbons){
+					for (rep in 1:numReplicates){
+						points(rtList[[peakGroup]][[item]][[isotop]][[rep]], intList[[peakGroup]][[item]][[isotop]][[rep]], type="l",col=fillColor[[isotop]])
+					}
+				}
+				legend("topright", plotOrder[item], bty="n", cex=.5)
+			}
+		}
+	}
+}
+
