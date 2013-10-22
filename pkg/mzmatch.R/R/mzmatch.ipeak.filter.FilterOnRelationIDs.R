8478fdf415b929077a878d9c697b2b680f1af8f7
ajankevics (Andris Jankevics) <andris.jankevics@manchester.ac.uk>
2013-10-22 11:39:26 +0000

    Fixed a bug related to Oracle Java 7 update 40
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mzmatchr@115 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mzmatch.R/R/mzmatch.ipeak.filter.FilterOnRelationIDs.R
+++ pkg/mzmatch.R/R/mzmatch.ipeak.filter.FilterOnRelationIDs.R
@@ -1,7 +1,7 @@
 mzmatch.ipeak.filter.FilterOnRelationIDs <- function(JHeapSize=1425,i=NULL, relationids=NULL, o=NULL, rejected=NULL, h=NULL, v=NULL)
 {
 	## define the java runtime parameters
-	java <- "java -da -dsa -Xmn1g -Xss160k -XX:+UseParallelGC -XX:ParallelGCThreads=10"
+	java <- "java -da -dsa -Xmn1g -Xss228k -XX:+UseParallelGC -XX:ParallelGCThreads=10"
 	## locate the mzmatch.jar file (it's included in the peakmlR package)
 	## JHeapSize - define amount of RAM availiable for java VM
 	JHeapSize <- paste(JHeapSize,"m",sep="")
