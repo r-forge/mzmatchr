090733f5a0cad09bfd4dbb936520d30e7e0af60e
ajankevics <ajankevics@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-29 19:32:20 +0000

    Fixed QCRSDFilter bug. Preperations for general version cleanup and update.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mzmatchr@128 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mzmatch.R/R/PeakML.QCRSDFilter.R
+++ pkg/mzmatch.R/R/PeakML.QCRSDFilter.R
@@ -1,33 +1,46 @@
-PeakML.QCRSDFilter <- function (filename, ionisation="detect", Rawpath=NULL, outputfile, QCSample=NULL, RSD=0.3, writeRejected=FALSE, sampleFraction=0.7)
+PeakML.QCRSDFilter2 <- function (filename, ionisation="detect", Rawpath=NULL, outputfile, QCSample=NULL, RSD=0.3, writeRejected=FALSE, sampleFraction=0.7)
 {
-	st <- system.time (PeakMLdata <- PeakML.Read (filename,ionisation,Rawpath))
+  
+  filename="gapfilled_chop_dclean.peakml"
+  ionisation="detect"
+  Rawpath=NULL
+  outputfile="gapfilled_chop_dclean_QCRSD.peakml"
+  QCSample="QC"
+  RSD=100
+  writeRejected=TRUE
+  sampleFraction=1
+	
+  
+  st <- system.time (PeakMLdata <- PeakML.Read (filename,ionisation,Rawpath))
 	PeakTable <- PeakML.Methods.getCompleteTable (PeakMLdata)
 	numberOfpeakSets <- ncol(PeakTable[[1]])
 
 	## Check which samples are QC's
+
 	qcsamples <- which (PeakMLdata$phenoData==QCSample)
 	if (length(qcsamples)<1)
 	{
 		stop ("No samples matching a given names for \"QC's\" are found. Please check if function parameter `QCSample` is set correctly.")
 	} 
 
-	Intensities.QC <- PeakTable[[1]][qcsamples,]
-	Intensities.QC <- rbind(Intensities.QC, NULL)
+	Intensities.QC <- PeakTable[[1]][qcsamples,] 
+	Intensities.QC <- rbind(Intensities.QC, NULL)  #
 	if (nrow(Intensities.QC)<2)
 	{
 		stop ("Only one QC sample is detected. QC RSD correction is not possible.")
 	}
 	
-	na.count <- apply(Intensities.QC,2,function (x) length(which(is.na(x))))
-	na.count <- 1-(na.count/nrow(Intensities.QC))
+	na.count <- apply(Intensities.QC,2,function (x) length(which(is.na(x))))   
+	na.count <- 1-(na.count/nrow(Intensities.QC)) 
 	
 
 	QC.RSD <- apply(Intensities.QC,2,var,na.rm=TRUE)
 	QC.RSD <- sqrt(QC.RSD)/apply(Intensities.QC,2,mean,na.rm=TRUE)
 	
 	to.remove <- which(is.na(QC.RSD))
-	to.remove <- append (to.remove, which(QC.RSD>=RSD | na.count >= sampleFraction))
-	to.remove <- unique(to.remove)
+	to.remove <- append(to.remove, which(QC.RSD>=RSD ))
+	to.remove <- append(to.remove, which(na.count < sampleFraction))   
+  	to.remove <- unique(to.remove)
 
 	## Write no matching
 	if (length(to.remove)>0 & writeRejected==TRUE)
